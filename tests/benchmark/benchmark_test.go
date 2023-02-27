1c1dd8e45093a8676aa98f044ed0bc495794dfab
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2023-02-28 01:31:37 +0900

    fix ci
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- tests/benchmark/benchmark_test.go
+++ tests/benchmark/benchmark_test.go
@@ -166,7 +166,7 @@ func BenchmarkHello(t *testing.B) {
 			}
 
 			// cleanup cache
-			assert.NilError(t, exec.Command("docker", "buildx", "prune", "-f").Run())
+			assert.NilError(t, exec.Command("docker", "buildx", "prune", "-f", "--keep-storage=5GB").Run())
 		})
 	}
 }
--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -382,7 +382,7 @@ func TestRuntimes(t *testing.T) {
 			assert.NilError(t, testCmd.Wait())
 
 			// cleanup cache
-			assert.NilError(t, exec.Command("docker", "buildx", "prune", "-f").Run())
+			assert.NilError(t, exec.Command("docker", "buildx", "prune", "-f", "--keep-storage=5GB").Run())
 		})
 	}
 }
